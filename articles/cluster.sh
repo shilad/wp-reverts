caddcfdc9e47d1637627c18f4f03a6a7fa05eecb
Shilad Sen <shilad@gmail.com>
2012-05-12 14:16:20 -0500

    Updates to scripts

--- articles/cluster.sh
+++ articles/cluster.sh
@@ -3,5 +3,9 @@
 mkdir -p bin
 
 CP=bin/:lib/trove-3.0.2.jar
-javac -cp $CP -sourcepath src/java/ -d bin/ src/java/wp/reverts/kmeans/*.java &&
-java -server -Xmx20000M -cp $CP wp.reverts.kmeans.Clusterer 10 1500 ./articles/translated/*.gz
+
+for k in 10 25 50 100 200 500 1000 2000; do
+    echo "===============DOING CLUSTERING $k==========================="
+    javac -cp $CP -sourcepath src/java/ -d bin/ src/java/wp/reverts/*/*.java &&
+    java -server -Xmx24000M -cp $CP wp.reverts.kmeans.Clusterer $k 20 articles/clusters.$k.txt ./articles/translated/*.gz
+done
