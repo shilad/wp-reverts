942f06a2a27fe168aa71503781424964253eb9a3
Shilad Sen <shilad@gmail.com>
2012-05-12 14:15:47 -0500

    Analysis scripts.

---
+++ scripts/test_graph.sh
@@ -0,0 +1,6 @@
+# cat articles/translate/*.gz >./doc_sims.txt.gz
+
+CP=bin/:`ls lib/*.jar | tr '\n' ':'`
+
+javac -cp $CP -sourcepath src/java/ -d bin/ src/java/wp/reverts/*/*.java &&
+java -server -Xmx24000M -cp $CP wp.reverts.core.RevertGraphBuilder dat/filtered_reverts.txt
